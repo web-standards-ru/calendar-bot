d5e52c68e425dc61bc5d8535952669fbe18298c1
dmitriym <dmitriym.09.12.1989@gmail.com>
2020-06-04 20:49:31 +0300

    lint

--- send.mjs
+++ send.mjs
@@ -2,16 +2,22 @@ import fs from 'fs';
 
 import yargs from 'yargs';
 
-import {
-    sendMsg
-} from './helpers/telegram.mjs';
+import { sendMsg } from './helpers/telegram.mjs';
 
 import WSEvent from './types/wsevent.mjs';
 
+/**
+ * CLI-интерфейс.
+ */
 export default async function () {
-    const events = yargs.argv._.map((file) => {
-        return sendMsg(WSEvent.fromYaml(fs.readFileSync(file, 'utf-8')), argv.token, argv.channel, argv.proxy);
-    });
+    const events = yargs.argv._.map(file =>
+        sendMsg(
+            WSEvent.fromYaml(fs.readFileSync(file, 'utf-8')),
+            yargs.argv.token,
+            yargs.argv.channel,
+            yargs.argv.proxy,
+        ),
+    );
 
     if (events.length == 0) {
         console.error('Events is empty!');
@@ -20,15 +26,13 @@ export default async function () {
 
     try {
         const results = await Promise.all(events);
-        if (!results.every((res) => {
-                return res.status == 200;
-            })) {
+        if (!results.every(res => res.status == 200)) {
             console.error(results);
-            return process.exit(2);
+            process.exit(2);
+        } else {
+            console.log(results);
+            process.exit(0);
         }
-
-        console.log(results);
-        process.exit(0);
     } catch (err) {
         console.error(err);
         process.exit(1);
